4f511a8b80965104a368cc53209aabe3fbae1e65
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-01-15 21:08:19 +0530

    Update Declarations.h

--- Declarations.h
+++ Declarations.h
@@ -20,6 +20,3 @@ typedef struct {
 
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair) ;
-
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
