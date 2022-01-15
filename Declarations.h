815a49300851d3cd83ebfd8fdac3edf7fcb40061
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-01-15 19:42:59 +0530

    Update Declarations.h

--- Declarations.h
+++ Declarations.h
@@ -2,8 +2,12 @@ enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 const int MAX_COLORPAIR_NAME_CHARS;
 
-const char* MajorColorNames[];
-const char* MinorColorNames[];
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
 
 int numberOfMajorColors;
 
