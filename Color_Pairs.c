34abc012a400f06f23965443d7cdc616e7a48e30
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-01-15 22:06:33 +0530

    Update Color_Pairs.c

--- Color_Pairs.c
+++ Color_Pairs.c
@@ -1,6 +1,11 @@
 #include "Var_Declarations.h"
 #include "Color_Pairs.h"
 
+numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);    
+numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
